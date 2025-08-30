0919ef50f1b80adbbd847d47968db0622b3ddf6b
danalmeidap <dbatistadealmeida23@gmail.com>
2025-08-30 10:37:13 -0300

    initial commit

---
+++ tests/test_app.py
@@ -0,0 +1,15 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from workout_project.app import app
+
+client = TestClient(app)
+
+
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        "message": "Welcome to the Workout Tracker API!"
+    }
