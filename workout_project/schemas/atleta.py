86911fedb99f93aa651ccffad4fcbf0711e7abc9
danalmeidap <dbatistadealmeida23@gmail.com>
2025-08-30 11:51:11 -0300

    feat:Atleta schema and model

---
+++ workout_project/schemas/atleta.py
@@ -0,0 +1,32 @@
+from typing import Annotated
+
+from pydantic import BaseModel, Field, PositiveFloat
+
+
+class Atleta(BaseModel):
+    nome: Annotated[
+        str,
+        Field(
+            description="Nome do atleta",
+            example="João Silva",
+            min_length=3,
+            max_length=50,
+        ),
+    ]
+    cpf: Annotated[
+        str,
+        Field(
+            description="CPF do atleta", example="12345678900", max_length=11
+        ),
+    ]
+    idade: Annotated[int, Field(description="Idade do atleta", example=25)]
+    peso: Annotated[
+        PositiveFloat, Field(description="Peso do atleta em kg", example=70.5)
+    ]
+    altura: Annotated[
+        PositiveFloat,
+        Field(description="Altura do atleta em metros", example=1.75),
+    ]
+    sexo: Annotated[
+        str, Field(description="Sexo do atleta", example="M", max_length=1)
+    ]
